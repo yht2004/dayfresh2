5a83be6e3bc6692f4b8ab76a668e737be1d16990
yht2004 <1921959159@qq.com>
2019-04-25 17:58:08 +0800

    增加商品列表、商品详情

--- good/urls.py
+++ good/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,re_path
-from good.views import IndexView,DetailView
+from good.views import IndexView,DetailView,ListView
 
 
 urlpatterns = [
 
     path('index',IndexView.as_view(),name='index'),#首页
     re_path(r'^good/(?P<goods_id>\d+)$',DetailView.as_view(),name='detail'),#商品详情页面
-
+    re_path(r'^list/(?P<type_id>\d+)/(?P<page>\d+)$',ListView.as_view(),name='list'),#商品列表页
 
 ]
